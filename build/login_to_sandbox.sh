ebf6588a66b88a5b85de9b1d5c9b10a9accb5df7
salakahim <fruela.palacio@apracor.com>
2022-04-25 14:19:26 +0200

    Update login sandbox  file

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -4,6 +4,17 @@ echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Server folder"
 ls ./keys
+cat ./keys/server.key
+
+echo "SANDBOX_CERT_KEY"
+echo $SANDBOX_CERT_KEY
+
+echo "SANDBOX_APP_KEY"
+echo $SANDBOX_APP_KEY
+
+echo "SANDBOX_USERNAME"
+echo $SANDBOX_USERNAME
+
 
 echo "Authenticating org"
 sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
