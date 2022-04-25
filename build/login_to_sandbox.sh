035af0570e17bc5eb2cc57e39da69d32c1273152
salakahim <fruela.palacio@apracor.com>
2022-04-25 16:32:32 +0200

    Update yaml  file, update the docker

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -4,3 +4,7 @@ echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
 echo "Authenticating org"
 sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub --instanceurl https://test.salesforce.com
+
+echo "Alias Org"
+echo $DevHub
+echo DevHub
