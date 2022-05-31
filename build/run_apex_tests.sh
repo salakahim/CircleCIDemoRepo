59a6c8f92040525a8c47ffb372ca06a9d67c2e19
salakahim <fruela.palacio@apracor.com>
2022-05-31 12:45:40 +0200

    remove the runing test

--- build/run_apex_tests.sh
+++ build/run_apex_tests.sh
@@ -1,7 +1,6 @@
 # Authenticate to salesforce
 #echo "Authenticating..."
 #sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
-
 #Create a scratch org
 #echo "Creating the Scratch Org..."
 #sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
